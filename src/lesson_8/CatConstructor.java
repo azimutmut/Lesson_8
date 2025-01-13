742212f4200e5534747935195d232105cffd1963
azimutmut <azimutmut@gmail.com>
2025-01-13 17:37:57 +0100

    updates

---
+++ src/lesson_8/CatConstructor.java
@@ -0,0 +1,21 @@
+//package lesson_8;
+//public class CatConstructor {
+//    public String fio;
+//    public String job;
+//    public String mail;
+//    public String phone;
+//    public int salary;
+//    public int age;
+//    public CatConstructor(String fio, String job, String mail, String phone, int salary, int age) {
+//        this.fio = fio;
+//        this.age = age;
+//        this.job = job;
+//        this.mail = mail;
+//        this.phone = phone;
+//        this.salary = salary;
+//        }
+//
+//    static void myMethod() {
+//    }
+//    }
+//
---
+++ src/lesson_8/MainApp.java
@@ -0,0 +1,13 @@
+package lesson_8;
+public class MainApp {
+    public static void main(String[] args){
+        Cat barsik = new Cat();
+        barsik.name = "Барсик";
+        barsik.color = "Белый";
+        barsik.age = 5;
+        barsik.meow();
+        barsik.info();
+
+    }
+}
+
